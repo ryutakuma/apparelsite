33d3ef572e6c266ee9b8660b51ff1071e776c810
ryu <ryu230527@eggnet.ac.jp>
2023-11-17 09:27:50 +0900

    12:09

--- src/search/views.py
+++ src/search/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    # 入力内容での検索をする
+    return render(request, "top.html")
+
+def category(request):
+    # カテゴリでの検索をする
     return render(request, "top.html")
